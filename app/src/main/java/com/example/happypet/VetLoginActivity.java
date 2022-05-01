e195cea14827671cb926330a20cc73d7bf56d1f9
Ruchini <ruchiniirushika@yahoo.com>
2022-05-01 21:21:23 +0530

    Vet login page created

--- app/src/main/java/com/example/happypet/VetLoginActivity.java
+++ app/src/main/java/com/example/happypet/VetLoginActivity.java
@@ -3,11 +3,11 @@ package com.example.happypet;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class VetLoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.Activity_Vet_Login);
     }
 }

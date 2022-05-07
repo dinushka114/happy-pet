0c0f49e88986af81eae17cad8e96a99ed35d0093
ruchini <ruchiniirushika@yahoo.com>
2022-05-07 22:46:52 +0530

    Vet class created

---
+++ app/src/main/java/com/example/happypet/dao/Vet.java
@@ -0,0 +1,19 @@
+package com.example.happypet.dao;
+
+public class Vet {
+    public String fullName ,clinicName, clinicAddress, clinicPhone, clinicHrs, email, password;
+    public Vet(){
+
+    }
+
+
+    public Vet(String fullName ,String clinicName,String clinicAddress,String clinicPhone,String clinicHrs,String email,String password){
+        this.fullName = fullName;
+        this.clinicName = clinicName;
+        this.clinicAddress = clinicAddress;
+        this.clinicPhone = clinicPhone;
+        this.clinicHrs = clinicHrs;
+        this.email = email;
+        this.password = password;
+    }
+}

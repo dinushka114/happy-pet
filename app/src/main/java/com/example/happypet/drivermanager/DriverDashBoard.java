db0e0da81219cc54e0865a05b5fef8a2b0a60fe8
dinushka114 <dinushkapiyumal678@gmail.com>
2022-05-07 11:42:12 +0530

    new branch created

---
+++ app/src/main/java/com/example/happypet/drivermanager/DriverDashBoard.java
@@ -0,0 +1,16 @@
+package com.example.happypet.drivermanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.happypet.R;
+
+public class DriverDashBoard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_dash_board);
+    }
+}

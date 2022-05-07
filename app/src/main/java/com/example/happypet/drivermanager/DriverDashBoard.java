d82106f278a1abe9817ca89f9f6887295ae8f575
ruchini <ruchiniirushika@yahoo.com>
2022-05-07 14:16:19 +0530

    pull reuest errors handled

--- app/src/main/java/com/example/happypet/drivermanager/DriverDashBoard.java
+++ app/src/main/java/com/example/happypet/drivermanager/DriverDashBoard.java
@@ -11,10 +11,9 @@ public class DriverDashBoard extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD:app/src/main/java/com/example/happypet/drivermanager/RegisterDriver.java
-=======
+
         setContentView(R.layout.activity_driver_dash_board);
->>>>>>> ca1d5488aa082126265562296a3eb8d0ee525bf8:app/src/main/java/com/example/happypet/drivermanager/DriverDashBoard.java
+
         //setContentView(R.layout.activity_register_driver);
     }
 }

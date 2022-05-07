0777b34935039db926e4abada0c31785933a0d70
ruchini <ruchiniirushika@yahoo.com>
2022-05-07 14:00:14 +0530

    vet navigationdrawer actions

--- app/src/main/java/com/example/happypet/VetDashboardActivity.java
+++ app/src/main/java/com/example/happypet/VetDashboardActivity.java
@@ -4,10 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -25,7 +27,7 @@ import org.w3c.dom.Text;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class VetDashboardActivity extends AppCompatActivity {
+public class VetDashboardActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private DrawerLayout vetDrawerLayout;
     private Toolbar vetToolBar;
@@ -35,6 +37,8 @@ public class VetDashboardActivity extends AppCompatActivity {
     private TextView vet_nav_fullName, vet_nav_vetEmail;
 
     private DatabaseReference userRef;
+
+
 
 
     @Override
@@ -48,6 +52,8 @@ public class VetDashboardActivity extends AppCompatActivity {
 
         vetDrawerLayout = findViewById(R.id.vetDrawerLayout);
         vetNav_view = findViewById(R.id.vetNav_view);
+
+        vetNav_view.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(VetDashboardActivity.this, vetDrawerLayout,
                 vetToolBar, R.string.vet_navigation_drawer_open, R.string.vet_navigation_drawer_close);
@@ -72,9 +78,7 @@ public class VetDashboardActivity extends AppCompatActivity {
                     String email = snapshot.child("email").getValue().toString();
                     vet_nav_vetEmail.setText(email);
 
-                    String imageUrl = snapshot.child("vetProfilePictureUrl").getValue().toString();
-                    Glide.with(getApplicationContext()).load(imageUrl).into(vet_nav_user_image);
-
+                    Glide.with(getApplicationContext()).load(snapshot.child("vetProfilePictureUrl").getValue().toString()).into(vet_nav_user_image);
                 }
             }
 
@@ -84,5 +88,23 @@ public class VetDashboardActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.vetProfile:
+                Intent intent =new Intent(VetDashboardActivity.this, VetProfileActivity.class);
+                startActivity(intent);
+
+
+        }
+        switch (item.getItemId()) {
+            case R.id.vetLogOut:
+                Intent intent = new Intent(VetDashboardActivity.this, SelectUserActivity.class);
+                startActivity(intent);
+        }
+        vetDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/happypet/vetandappointmentsmanage/VetProfileActivity.java
@@ -0,0 +1,15 @@
+package com.example.happypet.vetandappointmentsmanage;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.happypet.R;
+
+public class VetProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vet_profile);
+    }
+}
